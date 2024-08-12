581e3191e402f13ecfd6b3a8f0d848f1e58422c1
fourbarman <maks.java@yandex.ru>
2024-08-12 14:48:57 +0400

    XML-based configuration. Custom BeanPostProcessor - beforeInitialization method. Custom annotation process.

--- src/main/java/ru/fourbarman/database/repository/CompanyRepository.java
+++ src/main/java/ru/fourbarman/database/repository/CompanyRepository.java
@@ -1,15 +1,9 @@
 package ru.fourbarman.database.repository;
 
+import ru.fourbarman.bpp.InjectBean;
 import ru.fourbarman.database.pool.ConnectionPool;
 
 public class CompanyRepository {
-    private final ConnectionPool connectionPool;
-
-    public CompanyRepository(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
-    }
-
-    public static CompanyRepository of(ConnectionPool connectionPool) {
-        return new CompanyRepository(connectionPool);
-    }
+    @InjectBean
+    private ConnectionPool connectionPool;
 }
